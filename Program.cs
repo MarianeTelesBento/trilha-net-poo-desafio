6bb5438160e4fabab698c6feefb9fd29f7fcf715
Mariane Teles <marianebteles2021@hotmail.com>
2024-11-25 13:41:37 -0300

    "Desafio solucionado"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Console.WriteLine("\n");
+
+Smartphone nokia = new Nokia("11970848954", "Modelo 1", "12121213213", 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Minecraft");
+Console.WriteLine("\n");
+
+Smartphone iphone = new Iphone("2314210483", "Modelo 4", "3804809283", 16);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tiko e Teko");
+Console.WriteLine("\n");
